36a442ad64ed7b99a01520456646fc84e0b8f8cc
Osiris <potterosiris@gmail.com>
2020-09-13 20:54:03 -0400

    first commit

---
+++ build/precache-manifest.e9c48235ac719b53bdd4f693eb869919.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb91d54191e5d90ba75f2210ffc9a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c84c013ab6b4b0719712",
+    "url": "/static/css/main.ff1f9a4a.chunk.css"
+  },
+  {
+    "revision": "488ad05204ced621e113",
+    "url": "/static/js/2.d4a716f1.chunk.js"
+  },
+  {
+    "revision": "c84c013ab6b4b0719712",
+    "url": "/static/js/main.5b09e17b.chunk.js"
+  },
+  {
+    "revision": "97c20e3509b70f348612",
+    "url": "/static/js/runtime-main.bcd39b9f.js"
+  }
+]);
